bf8418cb59cf4a446541d63048d3e9d7a12eb3b9
JHOSEP ADBEL ROMERO LOA <jhromero.abx@gmail.com>
2022-03-23 15:32:25 -0500

    First WebApi DOTNET with entityFramework dotnet 6 https://www.youtube.com/watch?v=ZxAJrN7txtc

---
+++ web_api_users/Controllers/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using web_api_users.Controllers.Models;
+
+namespace web_api_users.Controllers.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
---
+++ web_api_users/Controllers/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace web_api_users.Controllers.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public bool Estado { get; set; }
+    }
+}
